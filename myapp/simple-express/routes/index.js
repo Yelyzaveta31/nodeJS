9157d0aa9c444b955ee055c269c81a8dee3e4a0f
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-06-25 11:45:52 +0300

    add structure

---
+++ myapp/simple-express/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+  res.render('response', { title: 'Simple express app', email, password });
+  });
+  
+  router.get('/:id', function (req, res, next) {
+    const { id } = req.params;
+    const contact = contacts.filter(el => el.id === id);
+    res.json(contact);
+    });
+    
+
+module.exports = router;
